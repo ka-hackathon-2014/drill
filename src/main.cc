13261e4f1ab3457a0b2450bff115a22465f6e3b0
Daniel J. Hofmann <daniel@trvx.org>
2014-05-24 11:27:59 +0200

    Eventing subsystem

--- src/main.cc
+++ src/main.cc
@@ -2,49 +2,36 @@
 #include <string>
 #include <vector>
 #include <iterator>
+#include <thread>
 
-#include <opencv2/objdetect/objdetect.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
+#include "queue.h"
+#include "event.h"
 
+
+using namespace drill;
 
 int main(int argc, char** argv)
 {
-  std::vector<std::string> args{argv, argv + argc};
-  if (args.size() != 2) return -1;
+  concurrent_queue<EvtMovementChange> extraction_q;
+  concurrent_queue<EvtEffect> classification_q;
 
-  cv::CascadeClassifier face_cascade;
-  face_cascade.load(args[1]);
+  std::thread extraction{[&] {
+    extraction_q.enqueue(EvtMovementChange{0, 0});
+    extraction_q.enqueue(EvtMovementChange{0, 0});
+    extraction_q.enqueue(EvtMovementChange{0, 0});
+  }};
 
-  cv::VideoCapture captureDevice;
-  captureDevice.open(0);
-
-  cv::Mat captureFrame;
-  cv::Mat grayscaleFrame;
-
-  cv::namedWindow("outputCapture", 1);
-
-  while (true) {
-    captureDevice >> captureFrame;
-
-    cv::cvtColor(captureFrame, grayscaleFrame, CV_BGR2GRAY);
-    cv::equalizeHist(grayscaleFrame, grayscaleFrame);
-
-    std::vector<cv::Rect> faces;
-
-    face_cascade.detectMultiScale(grayscaleFrame, faces, 1.1, 3, CV_HAAR_FIND_BIGGEST_OBJECT | CV_HAAR_SCALE_IMAGE,
-                                  cv::Size(30, 30));
-
-    for (const auto& face : faces) {
-      cv::Point pt1(face.x + face.width, face.y + face.height);
-      cv::Point pt2(face.x, face.y);
-
-      rectangle(captureFrame, pt1, pt2, cvScalar(0, 255, 0, 0), 1, 8, 0);
+  std::thread classification{[&] {
+    while (true) {
+      auto lst = extraction_q.dequeue();
+      if (lst.size())
+        std::cout << "foo" << std::endl;
     }
+  }};
 
-    cv::imshow("outputCapture", captureFrame);
+  std::thread sound{[&] {}};
 
-    if (cv::waitKey(30) >= 0)
-      break;
-  }
+  sound.join();
+  classification.join();
+  extraction.join();
 }
