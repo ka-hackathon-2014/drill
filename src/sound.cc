8df9e4450a030d7d49975fa471a16e8897a8f8c5
Marco Neumann <marco@crepererum.net>
2014-05-24 18:27:38 +0200

    Add counting sounds

--- src/sound.cc
+++ src/sound.cc
@@ -13,7 +13,30 @@ void run_sound(concurrent_queue<std::unique_ptr<EvtEffect>>& classification_q, s
 {
   Audioxx::Player player;
   std::vector<std::pair<std::string, std::vector<std::string>>> setup{
-      {"tooSlow", {"cola"}}, {"tooFast", {}}, {"height", {}}, {"count", {}}, {"ready", {}}};
+      {"tooSlow", {"cola"}},
+      {"tooFast", {}},
+      {"height", {}},
+      {"count_1", {"count_1_normal", "count_1_passiv_aggr", "count_1_aggr"}},
+      {"count_2", {"count_2_normal", "count_2_passiv_aggr", "count_2_aggr"}},
+      {"count_3", {"count_3_normal", "count_3_passiv_aggr", "count_3_aggr"}},
+      {"count_4", {"count_4_normal", "count_4_passiv_aggr", "count_4_aggr"}},
+      {"count_5", {"count_5_normal", "count_5_passiv_aggr", "count_5_aggr"}},
+      {"count_6", {"count_6_normal", "count_6_passiv_aggr", "count_6_aggr"}},
+      {"count_7", {"count_7_normal", "count_7_passiv_aggr", "count_7_aggr"}},
+      {"count_8", {"count_8_normal", "count_8_passiv_aggr", "count_8_aggr"}},
+      {"count_9", {"count_9_normal", "count_9_passiv_aggr", "count_9_aggr"}},
+      {"count_10", {"count_10_normal", "count_10_passiv_aggr", "count_10_aggr"}},
+      {"count_11", {"count_11_normal", "count_11_passiv_aggr", "count_11_aggr"}},
+      {"count_12", {"count_12_normal", "count_12_passiv_aggr"}},
+      {"count_13", {"count_13_normal", "count_13_passiv_aggr"}},
+      {"count_14", {"count_14_normal", "count_14_passiv_aggr"}},
+      {"count_15", {"count_15_normal", "count_15_passiv_aggr"}},
+      {"count_16", {"count_16_normal", "count_16_passiv_aggr"}},
+      {"count_17", {"count_17_normal", "count_17_passiv_aggr"}},
+      {"count_18", {"count_18_normal", "count_18_passiv_aggr"}},
+      {"count_19", {"count_19_normal", "count_19_passiv_aggr"}},
+      {"count_20", {"count_20_normal", "count_20_passiv_aggr"}},
+      {"ready", {}}};
   std::map<std::string, std::vector<Audioxx::Buffer>> buffers;
   for (const auto& type : setup) {
     std::vector<Audioxx::Buffer> variants;
@@ -31,16 +54,17 @@ void run_sound(concurrent_queue<std::unique_ptr<EvtEffect>>& classification_q, s
     auto lst = classification_q.dequeue();
     for (const auto& evt : lst) {
       std::string id = evt->getID();
-      const auto& variants = buffers.at(id);
       out()([&](std::ostream& out) { out << "Effect! (" << id << ")" << std::endl; });
 
       if (id == "count") {
         const auto& evtCount = dynamic_cast<const EvtCount&>(*evt);
-        size_t i = static_cast<size_t>(evtCount.n + 1);
-        if (i < variants.size()) {
-          player.play(variants.at(i), [&]()->bool { return shutdown; });
-        }
-      } else if (!variants.empty()) {
+        std::stringstream ss;
+        ss << "count_" << evtCount.n;
+        id = ss.str();
+      }
+
+      const auto& variants = buffers.at(id);
+      if (!variants.empty()) {
         std::uniform_int_distribution<size_t> dist{0, variants.size() - 1};
         const auto& buffer = variants.at(dist(rng));
         player.play(buffer, [&]()->bool { return shutdown; });
