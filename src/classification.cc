592cb73863896b6b3c3866660ae30eea6ce09509
Marco Neumann <marco@crepererum.net>
2014-05-24 22:05:00 +0200

    Basic calibration

--- src/classification.cc
+++ src/classification.cc
@@ -1,6 +1,8 @@
 #include "classification.h"
 
+#include <algorithm>
 #include <iostream>
+#include <list>
 #include <thread>
 
 namespace drill {
@@ -8,35 +10,59 @@ void run_classification(concurrent_queue<EvtMovementChange>& extraction_q,
                         concurrent_queue<std::unique_ptr<EvtEffect>>& classification_q, std::atomic<bool>& shutdown)
 {
   auto lastTp = std::chrono::system_clock::now();
+  bool calibrated = false;
+  std::list<double> caliTopLst;
+  std::list<double> caliBottomLst;
+  double caliTop;
+  double caliBottom;
   int count = 0;
   bool changed = false;
+
+  classification_q.enqueue(std::unique_ptr<EvtEffect>{new EvtCalibrate{}});
 
   while (!shutdown) {
     auto lst = extraction_q.dequeue();
     for (const auto& evt : lst) {
-      changed = true;
+      if (calibrated) {
+        changed = true;
 
-      // == 1. count ==
-      if (evt.sgn > 0) {
-        ++count;
-        if (count == 10) {
-          classification_q.enqueue(std::unique_ptr<EvtEffect>{new EvtReady{}});
-          shutdown = true;
-          return;
+        // == 1. count ==
+        if (evt.sgn > 0) {
+          ++count;
+          if (count == 10) {
+            classification_q.enqueue(std::unique_ptr<EvtEffect>{new EvtReady{}});
+            shutdown = true;
+            return;
+          } else {
+            classification_q.enqueue(std::unique_ptr<EvtEffect>{new EvtCount{count}});
+          }
+        }
+
+        // TODO 2. height
+
+        // == 3. too fast ==
+        auto nowTp = std::chrono::system_clock::now();
+        auto deltaMs = std::chrono::duration_cast<std::chrono::milliseconds>(nowTp - lastTp).count();
+        if (deltaMs < 300) {
+          classification_q.enqueue(std::unique_ptr<EvtEffect>{new EvtTooFast{}});
+        }
+        lastTp = std::chrono::system_clock::now();
+      } else {
+        // == 0. calibration ==
+        if (evt.sgn > 0) {
+          caliBottomLst.push_back(evt.y);
         } else {
-          classification_q.enqueue(std::unique_ptr<EvtEffect>{new EvtCount{count}});
+          caliTopLst.push_back(evt.y);
+        }
+
+        if ((caliTopLst.size() >= 2) && (caliBottomLst.size() >= 2)) {
+          caliTop = std::accumulate(caliTopLst.begin(), caliTopLst.end(), 0, std::plus<double>{}) /
+                    static_cast<double>(caliTopLst.size());
+          caliBottom = std::accumulate(caliBottomLst.begin(), caliBottomLst.end(), 0, std::plus<double>{}) /
+                       static_cast<double>(caliBottomLst.size());
+          calibrated = true;
         }
       }
-
-      // TODO 2. height
-
-      // == 3. too fast ==
-      auto nowTp = std::chrono::system_clock::now();
-      auto deltaMs = std::chrono::duration_cast<std::chrono::milliseconds>(nowTp - lastTp).count();
-      if (deltaMs < 300) {
-        classification_q.enqueue(std::unique_ptr<EvtEffect>{new EvtTooFast{}});
-      }
-      lastTp = std::chrono::system_clock::now();
     }
 
     if (changed) {
